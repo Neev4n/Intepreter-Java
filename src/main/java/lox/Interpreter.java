639ca920350fae3b5dd8e1a5efb5cf9d7a53156a
Neev4n <asszincrd@gmail.com>
2026-02-24 11:30:30 +1100

    Updated Interpreter to contain Stmt Visitor implementation to interpret statements

--- src/main/java/lox/Interpreter.java
+++ src/main/java/lox/Interpreter.java
@@ -1,6 +1,22 @@
 package lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
 
@@ -74,10 +90,15 @@ class Interpreter implements Expr.Visitor<Object> {
         return expr.accept(this);
     }
 
-    void interpret(Expr expression) {
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
         }
